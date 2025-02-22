9e775b19860df2aa6139d50479494c1db56a0296
ZenZxnZen <Rizeenroe00@gmail.com>
2025-02-21 21:58:29 -0500

    Trying npm run build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,35 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  
-  return(// const [name, setName] = useState('');
-    // const [message, setMessage] = useState('');
-  
-    // const handleSubmit = async (e) => {
-    //   e.preventDefault();
-    //   try {
-    //     const response = await axios.post('http://localhost:8000/api/data', { name });
-    //     setMessage(response.data.message);
-    //   } catch (error) {
-    //     console.error('Error posting data', error);
-    //   }
-    // };
-  
-    // return (
-    //   <div className="App">
-    //     <h1>React and Express</h1>
-    //     <form onSubmit={handleSubmit}>
-    //       <input
-    //         type="text"
-    //         value={name}
-    //         onChange={(e) => setName(e.target.value)}
-    //         placeholder="Enter your name"
-    //       />
-    //       <button type="submit">Submit</button>
-    //     </form>
-    //     {message && <p>{message}</p>}
-    //   </div>
-    // );
-    <div>
-      <h1>Upcoming... FR</h1>
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://zen-portfolio-git-main-rizeenroes-projects.vercel.app//api/data', { name });
+      setMessage(response.data.message);
+    } catch (error) {
+      console.error('Error posting data', error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>React and Express on Vercel</h1>
+      {/* <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <button type="submit">Submit</button>
+      </form> */}
+      {message && <p>{message}</p>}
     </div>
   );
 }
+
 
 export default App;
